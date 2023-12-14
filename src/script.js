96f206a264e50a3eb9eecacb526596d3a0de2f1e
Ameer <ameerapex496@gmail.com>
2023-12-14 08:44:25 +0100

    added logic + added styling

--- src/script.js
+++ src/script.js
@@ -1,8 +1,16 @@
 //selecting elements
 var headEl = document.getElementsByClassName("head")[0];
 // gelobal vraibales
-var imgCount = 0;
+var imgCount = 9;
 // functions
-var listImages = function () { };
+var listImages = function () {
+    for (var i = 0; i < imgCount; i++) {
+        var pageImg = document.createElement("img");
+        pageImg.src = "https://source.unsplash.com/random/300x30".concat(i);
+        pageImg.classList.add("page-Img");
+        headEl.appendChild(pageImg);
+    }
+};
+listImages();
 // eventlisnters
 window.addEventListener("lod", listImages);
--- src/script.ts
+++ src/script.ts
@@ -2,10 +2,18 @@
 const headEl = document.getElementsByClassName("head")[0];
 
 // gelobal vraibales
-let imgCount = 0;
+let imgCount = 9;
 
 // functions
-const listImages = () => {};
+const listImages = () => {
+  for (let i = 0; i < imgCount; i++) {
+    const pageImg = document.createElement("img");
+    pageImg.src = `https://source.unsplash.com/random/300x30${i}`;
+    pageImg.classList.add(`page-Img`);
+    headEl.appendChild(pageImg);
+  }
+};
 
+listImages();
 // eventlisnters
 window.addEventListener("lod", listImages);
