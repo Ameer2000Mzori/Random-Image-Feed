4ffbce0fb18c819d53c9fccbfc6b70aa21cf973d
Ameer <ameerapex496@gmail.com>
2023-12-14 08:38:26 +0100

    added comments , elements nedded selected

--- src/script.js
+++ src/script.js
@@ -1,1 +1,8 @@
-console.log("hallo world");
+//selecting elements
+var headEl = document.getElementsByClassName("head")[0];
+// gelobal vraibales
+var imgCount = 0;
+// functions
+var listImages = function () { };
+// eventlisnters
+window.addEventListener("lod", listImages);
--- src/script.ts
+++ src/script.ts
@@ -1,1 +1,11 @@
-console.log("hallo world");
+//selecting elements
+const headEl = document.getElementsByClassName("head")[0];
+
+// gelobal vraibales
+let imgCount = 0;
+
+// functions
+const listImages = () => {};
+
+// eventlisnters
+window.addEventListener("lod", listImages);
